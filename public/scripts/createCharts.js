9ab81accaeb33a0f7944cb73c78cbbb61c310382
Mohit Bajaj <mohit_b35@hotmail.com>
2020-07-21 19:20:56 +0530

    update createChart.js with chart generation logic - remove hardcoded chart - add code to load initial chart and update chart on category change - render chart (initial and update). Flow is as follows 1. renderChart is called with categoryId 2. within renderChart, we a. extract values for first/last day of selected month b. remove data from the existing pie chart c. call getChartData to fetch the chart data from API d. add data to the existing pie chart 3. getChartData calls Get Chart Data route and returns formatted data 4. addData/removeData update the chart object and then update the chart

--- public/scripts/createCharts.js
+++ public/scripts/createCharts.js
@@ -1,12 +1,82 @@
 let ctx = document.getElementById('piechart');
+let chartCategorySelector = document.getElementById('chart-category');
+chartCategorySelector.addEventListener('change', updateChart);
 
 let pieChart = new Chart(ctx, {
 	type: 'pie',
 	data: {
-		"labels":["Red","Blue","Yellow"],
-		"datasets":[{
-			"label":"My First Dataset",
-			"data":[300,50,100],
-			"backgroundColor":["rgb(255, 99, 132)","rgb(54, 162, 235)","rgb(255, 205, 86)"]
-		}]}
+		"labels":[],
+		"datasets":[]
+	}
 });
+
+function loadInitialChart() {
+	console.log("Loading initial chart");
+	let categoryId = chartCategorySelector.value;
+	renderChart(categoryId);
+}
+
+loadInitialChart();
+
+async function renderChart(categoryId) {
+	let { firstDay, lastDay} = monthBounds(monthSelector.value);
+	console.log("Rendering chart for category id:",categoryId);
+	removeData(pieChart);
+	let { title, labels, data, backgroundColor } = await getChartData(categoryId, firstDay, lastDay);
+	addData(pieChart, labels, title, data, backgroundColor);
+	
+}
+
+function removeData(chart) {
+	console.log("Clearing chart data");
+    chart.data.labels = [];
+    chart.data.datasets = [];
+    chart.update();
+};
+
+function addData(chart, labels, title, data, backgroundColor) {
+    chart.data.labels = labels;
+	chart.data.datasets.push({
+		label: title,
+		data: data,
+		backgroundColor: backgroundColor
+	})
+    chart.update();
+}
+
+async function getChartData(categoryId, firstDay, lastDay) {
+	let chartData = await axios.get(`spend/summary/${categoryId}?from=${firstDay}&to=${lastDay}`);
+
+	let title = `Category: ${chartData.data.categoryName}`;
+
+	let isCategory = (chartData.data.type === "subcategories") ? true : false;
+
+	let labels = chartData.data.data.map((item)=>{
+		if (isCategory){
+			return item['sub_category_name'];
+		} else {
+			return item['category_name'];
+		}
+	});
+
+	let data = chartData.data.data.map((item)=>{
+		return item.total;
+	});
+
+	let backgroundColor = chartData.data.data.map((item)=>{
+		return item.color;
+	});
+
+	return {
+		title, 
+		labels,
+		data,
+		backgroundColor
+	};
+}
+
+function updateChart(event) {
+	console.log("In update chart");
+	let categoryId = event.target.value;
+	renderChart(categoryId);
+}
